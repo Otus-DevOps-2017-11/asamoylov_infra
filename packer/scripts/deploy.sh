9189b54dbf2a465a3f2353869c10a3c4fc88df67
drenjy <drenjy@narod.ru>
2018-03-08 13:42:38 +0300

    Выполнение Д.З № 13

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+set -fuCe -o pipefail
+
 cd ~
-git clone https://github.com/Otus-DevOps-2017-11/reddit.git
-cd reddit && bundle install
+
+git clone https://github.com/Otus-DevOps-2017-11/reddit.git reddit
+cd reddit
 bundle install
-puma -d
-if ps aux | grep puma | grep -vq grep; then
-  echo "Puma start"
-else
-  echo "Puma stop"
-fi
+
+sudo mv /tmp/pumad.service /etc/systemd/system/pumad.service
+sudo systemctl enable pumad.service
